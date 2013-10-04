f2ed43ce5e68ac63eda7ec24ae09c032d95c9192
Todd Berman <tberman@thefactory.com>
2013-10-03 21:23:01 -0700

    reformat

--- SocketRocket/StructsAndEnums.cs
+++ SocketRocket/StructsAndEnums.cs
@@ -1,11 +1,10 @@
 using System;
 
-namespace SocketRocket
-{
-	public enum SRReadyState {
-		SR_CONNECTING = 0,
-		SR_OPEN = 1,
-		SR_CLOSING = 2,
-		SR_CLOSED = 3
-	}
+namespace SocketRocket {
+    public enum SRReadyState {
+        SR_CONNECTING = 0,
+        SR_OPEN = 1,
+        SR_CLOSING = 2,
+        SR_CLOSED = 3
+    }
 }
--- TestChat/TestChatViewController.cs
+++ TestChat/TestChatViewController.cs
@@ -1,112 +1,103 @@
 using System;
 using System.Drawing;
 using System.Collections.Generic;
-
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
-
 using SocketRocket;
 
-namespace TestChat
-{
-	public partial class TestChatViewController : UITableViewController
-	{
-		SRWebSocket socket;
-		List<ChatMessage> messages = new List<ChatMessage>();
+namespace TestChat {
+    public partial class TestChatViewController : UITableViewController {
+        SRWebSocket socket;
+        List<ChatMessage> messages = new List<ChatMessage>();
 
-		public TestChatViewController () : base ("TestChatViewController", null)
-		{
-		}
+        public TestChatViewController () : base("TestChatViewController", null) {
+        }
 
-		public TestChatViewController(IntPtr handle) : base(handle) {}
-		
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
+        public TestChatViewController (IntPtr handle) : base(handle) {
+        }
+
+        public override void DidReceiveMemoryWarning() {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
 			
-			// Release any cached data, images, etc that aren't in use.
-		}
-		
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad() {
+            base.ViewDidLoad();
 			
-			// Perform any additional setup after loading the view, typically from a nib.
+            // Perform any additional setup after loading the view, typically from a nib.
 
-			socket = new SRWebSocket(new NSUrl("ws://localhost:9000/chat"));
-			socket.MessageReceived += HandleMessageReceived;
-			socket.Open();
+            socket = new SRWebSocket(new NSUrl("ws://localhost:9000/chat"));
+            socket.MessageReceived += HandleMessageReceived;
+            socket.Open();
 
-			TableView.DataSource = new TestChatDataSource(messages);
+            TableView.DataSource = new TestChatDataSource(messages);
 
-			inputView.BecomeFirstResponder();
+            inputView.BecomeFirstResponder();
 
-			inputView.Changed += HandleInputChanged;
-		}
+            inputView.Changed += HandleInputChanged;
+        }
 
-		void HandleInputChanged (object sender, EventArgs e)
-		{
-			if (inputView.Text.EndsWith("\n")) {
-				var msg = inputView.Text.TrimEnd ();
-				messages.Add (new ChatMessage { Message = msg, Mine = true });
-				TableView.InsertRows(new NSIndexPath[] { NSIndexPath.FromRowSection(messages.Count - 1, 0) }, UITableViewRowAnimation.None);
-				socket.Send (new NSString(msg));
-				inputView.Text = "";
-			}
-		}
+        void HandleInputChanged(object sender, EventArgs e) {
+            if (inputView.Text.EndsWith("\n")) {
+                var msg = inputView.Text.TrimEnd();
+                messages.Add(new ChatMessage { Message = msg, Mine = true });
+                TableView.InsertRows(new NSIndexPath[] { NSIndexPath.FromRowSection(messages.Count - 1, 0) }, UITableViewRowAnimation.None);
+                socket.Send(new NSString(msg));
+                inputView.Text = "";
+            }
+        }
 
-		void HandleMessageReceived (object sender, SRMessageReceivedEventArgs e)
-		{
-			messages.Add(new ChatMessage { Message = e.Message.ToString(), Mine = false });
-			TableView.InsertRows(new NSIndexPath[] { NSIndexPath.FromRowSection(messages.Count - 1, 0) }, UITableViewRowAnimation.None);
-		}
-		
-		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
-		{
-			// Return true for supported orientations
-			return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
-		}
-	}
+        void HandleMessageReceived(object sender, SRMessageReceivedEventArgs e) {
+            messages.Add(new ChatMessage {
+                Message = e.Message.ToString(),
+                Mine = false
+            });
+            TableView.InsertRows(new NSIndexPath[] { NSIndexPath.FromRowSection(messages.Count - 1, 0) }, UITableViewRowAnimation.None);
+        }
 
-	class ChatMessage
-	{
-		public string Message { get; set; }
-		public bool Mine { get; set; }
-	}
+        public override bool ShouldAutorotateToInterfaceOrientation(UIInterfaceOrientation toInterfaceOrientation) {
+            // Return true for supported orientations
+            return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+        }
+    }
 
-	class TestChatDataSource : UITableViewDataSource
-	{
-		private List<ChatMessage> messages;
+    class ChatMessage {
+        public string Message { get; set; }
 
-		public TestChatDataSource(List<ChatMessage> msgs)
-		{
-			messages = msgs;
-		}
+        public bool Mine { get; set; }
+    }
 
-		#region implemented abstract members of UITableViewDataSource
+    class TestChatDataSource : UITableViewDataSource {
+        private List<ChatMessage> messages;
 
-		public override int RowsInSection (UITableView tableView, int section)
-		{
-			return messages.Count;
-		}
+        public TestChatDataSource (List<ChatMessage> msgs) {
+            messages = msgs;
+        }
 
-		public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
-		{
-			UITableViewCell cell = tableView.DequeueReusableCell("CELL");
+        #region implemented abstract members of UITableViewDataSource
 
-			if (cell == null) {
-				cell = new UITableViewCell(UITableViewCellStyle.Default, "CELL");
-			}
+        public override int RowsInSection(UITableView tableView, int section) {
+            return messages.Count;
+        }
 
-			cell.TextLabel.Text = messages[indexPath.Row].Message;
-			cell.TextLabel.BackgroundColor = UIColor.Clear;
-			cell.ContentView.BackgroundColor = messages[indexPath.Row].Mine ? UIColor.Purple : UIColor.Green;
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath) {
+            UITableViewCell cell = tableView.DequeueReusableCell("CELL");
 
-			return cell;
-		}
+            if (cell == null) {
+                cell = new UITableViewCell(UITableViewCellStyle.Default, "CELL");
+            }
 
-		#endregion
-	}
+            cell.TextLabel.Text = messages[indexPath.Row].Message;
+            cell.TextLabel.BackgroundColor = UIColor.Clear;
+            cell.ContentView.BackgroundColor = messages[indexPath.Row].Mine ? UIColor.Purple : UIColor.Green;
+
+            return cell;
+        }
+
+        #endregion
+
+    }
 }
 
